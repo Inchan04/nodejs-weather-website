56fe188a8bd0a68e2088d2d7d93c6cbe98af53bc
unknown <briosoc22@sorsu.edu.ph>
2021-10-29 20:10:12 +0800

    add humidity and fixing body in geo

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
 	const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiaW5jaGFuMDQiLCJhIjoiY2t1eXQ3dmxyMGttYTJ2dGMzemN5N2IzdyJ9.pP-moJd3-1Kf1-1jFzbhFA&limit=1'
 
-    request({ url, json: true}, (error, { body }) => {
+    request({ url, json: true}, (error, { body: body }) => {
         if (error){
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
